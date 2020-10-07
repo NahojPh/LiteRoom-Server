30cecfcef36dbf526e1720fd77749906592e3274
NahojPh <johan.philip04@gmail.com>
2020-10-07 15:38:59 +0200

    Moved main.dart from bin to src.

--- src/main.dart
+++ src/main.dart
@@ -7,19 +7,20 @@ Future main () async {
   await HttpServer.bind("192.168.1.3", 25565)
   ..listen((HttpRequest request) async {
 
-    if (request.requestedUri.path == "/api") { //if recived a post or get request on the /api route then go ahead
+    if (request.requestedUri.path == "/api") { //if recived a post or get request on the /api route then go ahead.
 
-      if (request.method == "POST") { // post method
+      if (request.method == "POST") { // post method. Post handeler starts here.
         Map<dynamic, dynamic> data = request.uri.queryParameters;
-        if (data["r"].hashCode != null && data["g"] != null && data["b"] != null) { //if colors are specified then go ahead and run the script. 
-          Process.start("./lightscripts/lightswitch.py", ["${data["power"]}", "${data["r"]}", "${data["g"]}", "${data["b"]}"]);
+        if (data["r"] != null && data["g"] != null && data["b"] != null) { //if colors are specified then go ahead and run the script.
+          print(data);
+          //Process.start("./lightscripts/lightswitch.py", ["${data["power"]}", "${data["r"]}", "${data["g"]}", "${data["b"]}"]);
         }
         else {
           request.response.write("Recived bad parameters: ${data}");
         }
       }
       else if (request.method == "GET") { //get method
-
+        request.response.write("${DateTime.now()}");
       }
       else {
         request.response.statusCode = 400;
