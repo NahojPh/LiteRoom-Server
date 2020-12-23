b61d63d186fc028c6727f2755e5ec676d556449a
NahojPh <johan.philip04@gmail.com>
2020-12-23 17:34:25 +0100

    Added save-to-file so when a post reqeust is recived the data goes into lightData.json

--- src/main.dart
+++ src/main.dart
@@ -3,42 +3,48 @@ import 'dart:async';
 import 'dart:convert';
 
 Future main() async {
-  int color = 0x12;
-  bool power = false;
+
+  File file = File("lightData.json");
+  Map<dynamic, dynamic> fileData = json.decode(file.readAsStringSync());
+
+  int color = fileData["color"];
+  bool power = fileData["power"];
 
   print("Started");
   await HttpServer.bind("192.168.1.3", 25565)
     ..listen((HttpRequest request) async {
-      if (request.requestedUri.path == "/api") {
-        //if recived a post or get request on the /api route then go ahead and continue.
-
-        if (request.method == "POST") {
-          // post method.
+      print("listeing");
+      if (request.requestedUri.path == "/api") { //if recived a post or get request on the /api route then go ahead and continue.
+        
+        print("inside /api");
+        if (request.method == "POST") { // post method.
+        print("post request recv");
           Map<dynamic, dynamic> data = request.uri.queryParameters;
-          if (data["color"] != null) {
-            //if colors are specified then go ahead and run the script.
-            color = int.parse(
-                data["color"]); //sets the color varible to the queryParameters.
+          if (data["color"] != null) { //if colors are specified then go ahead and run the script.
+           
+            color = int.parse(data["color"]); //sets the color varible to the queryParameters.
+            file.writeAsString(fileData.toString());
+            print("colors gotten: $color");
             //Process.start("./lightscripts/lightswitch.py", ["${data["power"]}", "${data["r"]}", "${data["g"]}", "${data["b"]}"]);
-          } else {
+          }
+          else {
             request.response.write("Recived bad parameters: ${data}");
           }
-        } else if (request.method == "GET") {
-          //get method.
+        }
+        else if (request.method == "GET") { //get method.
+         
           print("Get request recived.");
-          Map<dynamic, dynamic> body = {
-            //creates a body for the get response with all the right key:pair values.
+          Map<dynamic, dynamic> body = { //creates the body for the get response with all the right key:pair values.
             "power": power,
             "color": color,
           };
           request.response.write(json.encode(body));
-        } else {
+        } 
+        else {
           request.response.statusCode = 400;
-          request.response.write(
-              "Recived an unsupported request method: ${request.method}"); //if server gets an unsupported request type then send back a status code of 400(Bad request).
+          request.response.write("Recived an unsupported request method: ${request.method}"); //if server gets an unsupported request type then send back a status code of 400(Bad request).
         }
       }
-
       request.response.close();
     });
 }
